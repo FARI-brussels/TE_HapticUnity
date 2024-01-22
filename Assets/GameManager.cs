8b81eb88cbb30811cca6b93fa3fda2979d2ff9d4
fari <susheelnath1996@gmail.com>
2024-01-22 12:59:23 +0100

    feat: structured scene to final setup

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject hapticSetup;
+
+    void Start()
+    {
+        hapticSetup.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
